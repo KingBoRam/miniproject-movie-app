872a80699f35b00cb1d3906f8e0986cbff09071d
김보람 <bory2321@naver.com>
2024-06-10 19:57:48 +0900

    배포

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mini-movie-app-54bdd.firebaseapp.com",
-  projectId: "mini-movie-app-54bdd",
-  storageBucket: "mini-movie-app-54bdd.appspot.com",
-  messagingSenderId: "721337279705",
-  appId: "1:721337279705:web:982546ecb33f3badd28ac8",
-  measurementId: "G-QVEF001M7N",
+  authDomain: "themovies-a075b.firebaseapp.com",
+  projectId: "themovies-a075b",
+  storageBucket: "themovies-a075b.appspot.com",
+  messagingSenderId: "267771447145",
+  appId: "1:267771447145:web:738c5132b3ba2de7999ff8",
 };
 
 // Initialize Firebase
