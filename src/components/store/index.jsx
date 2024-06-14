9e25348f7fb8c1ec64dfaff2990910d0f2f5d43e
김보람 <bory2321@naver.com>
2024-06-15 03:09:13 +0900

    localstorage 저장 위해서 redux-persist 추가

--- src/components/store/index.jsx
+++ src/components/store/index.jsx
@@ -1,12 +1,32 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {bookmarkReducer} from "./bookmarkSlice";
-import {userNameReducer} from "./userNameSlice";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // 기본적으로 localStorage를 사용합니다.
+import {bookmarkReducer} from "./bookmarkSlice"; // bookmark 리듀서
+import {userNameReducer} from "./userNameSlice"; // userName 리듀서
 
-const store = configureStore({
-  reducer: {
-    bookmark: bookmarkReducer,
-    userName: userNameReducer,
-  },
+// persist 설정
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["bookmark"], // bookmark 리듀서만 localStorage에 저장합니다.
+};
+
+const rootReducer = combineReducers({
+  bookmark: bookmarkReducer,
+  userName: userNameReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({serializableCheck: false}),
+  // 기본 값이 true지만 배포할때 코드를 숨기기 위해서 false로 변환하기 쉽게 설정에 넣어놨다.
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
 export default store;
+
+// 직렬화 오류 해결 https://velog.io/@pest95/RTK-non-serializable-value-%EC%98%A4%EB%A5%98-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import App from "./App.jsx";
 import "./index.css";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import store from "./components/store/index.jsx";
+import store, {persistor} from "./components/store/index.jsx";
+import {PersistGate} from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
+
+// loading = null -> 정보 로딩 중에는 아무것도 표시하지 않는다. 특정 컴포넌트를 넣어 줄수 있음.
