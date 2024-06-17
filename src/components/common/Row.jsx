1067fd0790bec59644429f48c5d09761492b0419
김보람 <bory2321@naver.com>
2024-06-17 11:19:01 +0900

    change swiper-bullet opacity

--- src/components/common/Row.jsx
+++ src/components/common/Row.jsx
@@ -13,30 +13,42 @@ import "swiper/css/navigation";
 // import required modules
 import {EffectCoverflow, Pagination, Navigation} from "swiper/modules";
 import {useNavigate} from "react-router-dom";
-import {useSelector} from "react-redux";
+// import {useSelector} from "react-redux";
 
 const Row = () => {
   const [topMovies, setTopMovies] = useState([]);
-  const isDarkMode = useSelector((state) => state.theme.isDarkMode);
 
-  useEffect(() => {
-    const bulletSelectors = [
-      ".swiper-pagination-bullet-active-prev",
-      ".swiper-pagination-bullet-active-prev-prev",
-      ".swiper-pagination-bullet-active-next",
-      ".swiper-pagination-bullet-active-next-next",
-    ];
-    bulletSelectors.forEach((selector) => {
-      const element = document.querySelector(selector);
-      if (element) {
-        if (isDarkMode) {
-          element.classList.add("swiper-bullet-dark-mode");
-        } else {
-          element.classList.remove("swiper-bullet-dark-mode");
-        }
-      }
-    });
-  }, [isDarkMode]);
+  // 이 컴포넌트에 주석처리 된 부분은, darkmode일때 클래스네임을 추가해서 색상을 변경시키기 위함인데
+  // 이게 새로고침시에는 적용이 안된다. 그러다보니까 뭐 oninit했을 때 추가하고 어쩌고 하는
+  // 복잡한 과정이 점점 추가되어야했다. 아마도 리액트인데 dom을 직접 조작하다보니 문제가 생겼던게
+  // 아닐까 싶었다 (참조 : https://sylagape1231.tistory.com/59)
+  // 그리고 지피티를 통해서 수정하다보니 점점 코드가 내가 이해할 수 없는 방향으로 흘러서 모두 일단 지웠다.
+  // 근본적으로 bullet에 opacity가 있어서 문제였던거라 그냥 opacity를 지워서 문제는 해결했지만
+  // 이후에 클래스네임을 또 추가하고 싶은 상황이 있을수도 있겠다 싶어서 남겨둠
+  // const isDarkMode = useSelector((state) => state.theme.isDarkMode);
+
+  // const updateBulletClasses = useCallback(() => {
+  //   const bulletSelectors = [
+  //     ".swiper-pagination-bullet-active-prev",
+  //     ".swiper-pagination-bullet-active-prev-prev",
+  //     ".swiper-pagination-bullet-active-next",
+  //     ".swiper-pagination-bullet-active-next-next",
+  //   ];
+  //   bulletSelectors.forEach((selector) => {
+  //     const elements = document.querySelectorAll(selector);
+  //     elements.forEach((element) => {
+  //       if (isDarkMode) {
+  //         element.classList.add("swiper-bullet-dark-mode");
+  //       } else {
+  //         element.classList.remove("swiper-bullet-dark-mode");
+  //       }
+  //     });
+  //   });
+  // }, [isDarkMode]);
+
+  // useEffect(() => {
+  //   updateBulletClasses();
+  // }, [isDarkMode, updateBulletClasses]);
 
   useEffect(() => {
     axios
@@ -45,8 +57,6 @@ const Row = () => {
   }, []);
 
   const navigate = useNavigate();
-
-  // https://swiperjs.com/demos
 
   return (
     <div className="row-container">
@@ -70,7 +80,20 @@ const Row = () => {
         pagination={{
           dynamicBullets: true,
         }}
-        className="mySwiper">
+        className="mySwiper"
+        // onInit={(swiper) => {
+        //   // Swiper 초기화 후 bullet 클래스 업데이트
+        //   updateBulletClasses();
+        //   // 초기화 후 bullet 클래스를 동적으로 변경하기 위해 Observer를 사용
+        //   const observer = new MutationObserver(updateBulletClasses);
+        //   swiper.pagination.el
+        //     .querySelectorAll(".swiper-pagination-bullet")
+        //     .forEach((bullet) => {
+        //       observer.observe(bullet, {attributes: true});
+        //     });
+        // }
+        // }
+      >
         {topMovies.map((item) => (
           <SwiperSlide key={item.id}>
             <img
